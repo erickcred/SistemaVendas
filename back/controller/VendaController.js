8f4536e6761d71c55090e3a0936ce1a9bd56172c
Erick Rick <erickcredjes@gmail.com>
2022-02-03 19:37:48 -0300

    Alterando o mode de vanda

--- back/controller/VendaController.js
+++ back/controller/VendaController.js
@@ -1,14 +1,45 @@
 const VendaModel = require("../models/venda");
+const DetalheVendaModel = require("../models/detalheVenda");
+const ProdutoModel = require("../models/produto");
 
 function createVenda(req, res) {
     const data = req.body;
 
     try {
         VendaModel.create({
-            idCliente: data.idCliente, idUser: data.idUser, dataVenda: data.dataVenda
+            idCliente: data.idCliente, idUser: data.idUser
         }, (erro, venda_data) => {
             if (venda_data) {
-                res.status(201).send({ message: "Venda Finalizada com sucesso!", venda: venda_data });
+
+                let detalhes = data.detalheVenda;
+                detalhes.forEach((element, index) => {
+                    const detalhe = new DetalheVendaModel();
+                    detalhe.idProduto = element.idProduto;
+                    detalhe.quantidade = element.quantidade;
+                    detalhe.idVenda = venda_data._id
+
+                    detalhe.save((erro, detalhe_data) => {
+                        if (detalhe_data) {
+
+                            ProdutoModel.findById({_id: element.idProduto }, (erro, produto_data) => {
+                                if (produto_data && produto_data.estoque >= element.estoque) {
+
+                                    ProdutoModel.findByIdAndUpdate({ _id: produto_data._id}, { estoque: parseInt(produto_data.estoque) - parseInt(element.quantidade) }, (erro, estoque_data) => {
+                                       if (erro) throw erro; res.status(201).send({ message: "Venda Finalizada com sucesso!", venda: venda_data });
+                                        
+                                    });
+                                    
+                                } else {
+                                    res.status(404).send({ message: `Estoque insuficiente para essa venda, quantidade disponivel é de: ${produto_data.estoque}, quantidade deste item na venda: ${element.quantidade}` });
+                                }
+                            });
+
+                        } else {
+                            res.status(404).send({ message: "Não foi possível finalizar a venda!" });
+                        }
+                    })
+                });
+
             } else {
                 res.status(404).send( { message: "Não foipossível finalizar a venda!" });
             }
