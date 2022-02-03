f33d6c07479f643e0e60d629a60b118751d7acee
Erick Rick <erickcredjes@gmail.com>
2022-02-03 18:30:45 -0300

    Alteração em models/detalheVenda.js

--- back/models/detalheVenda.js
+++ back/models/detalheVenda.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const DetalheVendaSchema = Schema({
     idProduto: { type: Schema.ObjectId, ref: "produto" },
-    quantidade: Number
+    quantidade: Number,
+    idVenda: { type: Schema.ObjectId, ref: "venda"}
 });
 
 module.exports = mongoose.model("detalheVenda", DetalheVendaSchema);
